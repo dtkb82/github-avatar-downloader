12623dad0117343d861abe0bd6e60639ffdab799
Kenneth <krbrown@live.ca>
2018-01-23 01:02:12 +0000

    get request with hardcoded parameters

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+// console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, callback) {
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    callback(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+	console.log("Errors:", err);
+	console.log("Result:", result);
+});
